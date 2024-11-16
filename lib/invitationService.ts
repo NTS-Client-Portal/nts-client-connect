886327b1832de27ba35029b6a8f163592c0744ba
adamn1225 <anoah1225@gmail.com>
2024-11-16 15:53:45 -0600

    Fix line endings and type errors

--- lib/invitationService.ts
+++ lib/invitationService.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import { supabase } from './supabaseClient'; // Adjust the import path as needed
+import { supabase } from './initSupabase'; // Adjust the import path as needed
 
 export const sendInvitations = async (emails: string[], userId: string, companyId: string) => {
     for (const email of emails) {
