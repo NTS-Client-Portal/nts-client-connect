61e8654cfec42629590ab239d3c9df009dfde72f
adamn1225 <anoah1225@gmail.com>
2024-12-05 18:50:27 -0500

    side nav fix

--- pages/nts/sales/_components/layout/SalesSideNav.tsx
+++ pages/nts/sales/_components/layout/SalesSideNav.tsx
@@ -56,13 +56,13 @@ const SalesSideNav: React.FC<SalesSideNavProps> = ({ isSidebarOpen, toggleSideba
                         <h3>Welcome {userProfile?.first_name || 'User'}</h3>
                     </div>
                     <ul className='flex gap-3 flex-col flex-grow space-y-1 overflow-y-hidden'>
-                        <li className={`w-full flex justify-normal m-0 ${currentView === 'quote-requests' ? "active" : ""}`}>
                         <li className={`w-full flex justify-normal m-0 ${currentView === 'crm' ? "active" : ""}`}>
                             <button onClick={() => setCurrentView('crm')} className={`side-nav-btn text-stone-100 font-semibold py-1 w-full ${currentView === 'crm' ? "active" : ""}`}>
                                 <span className='w-full flex items-center flex-nowrap justify-normal gap-2'><ChartArea size={'20px'} /> <span className='text-xs md:text-sm'>Shipper&apos;s</span></span>
                             </button>
                         </li>
-                            <button onClick={() => setCurrentView('salesdash')} className={`side-nav-btn text-stone-100 font-semibold py-1 w-full ${currentView === 'quote-requests' ? "active" : ""}`}>
+                        <li className={`w-full flex justify-normal m-0 ${currentView === 'salesdash' ? "active" : ""}`}>
+                            <button onClick={() => setCurrentView('salesdash')} className={`side-nav-btn text-stone-100 font-semibold py-1 w-full ${currentView === 'salesdash' ? "active" : ""}`}>
                                 <span className='flex items-center flex-nowrap justify-normal gap-2'><Workflow size={'20px'} /> <span className='text-xs md:text-sm'>Future Shipper&apos;s Static Page</span></span>
                             </button>
                         </li>
