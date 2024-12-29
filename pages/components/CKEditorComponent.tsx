e7d665513eccaa74b9ecd50da8a064dc7281c5bc
adamn1225 <anoah1225@gmail.com>
2024-12-29 16:33:36 -0500

    ckeditor fight

--- pages/components/CKEditorComponent.tsx
+++ pages/components/CKEditorComponent.tsx
@@ -83,253 +83,251 @@ const CKEditorComponent: React.FC<CKEditorComponentProps> = ({ content, setConte
         return () => setIsLayoutReady(false);
     }, []);
 
-    const { editorConfig } = useMemo(() => {
+    const editorConfig = useMemo(() => {
         if (!isLayoutReady) {
             return {};
         }
 
         return {
-            editorConfig: {
-                toolbar: {
-                    items: [
-                        'sourceEditing',
-                        'showBlocks',
-                        '|',
-                        'heading',
-                        'style',
-                        'fontSize',
-                        'fontFamily',
-                        'fontColor',
-                        'fontBackgroundColor',
-                        'bold',
-                        'italic',
-                        'underline',
-                        'link',
-                        'insertImage',
-                        'insertTable',
-                        'highlight',
-                        'blockQuote',
-                        'alignment',
-                        'bulletedList',
-                        'numberedList',
-                        'todoList',
-                        'outdent',
-                        'indent'
-                    ],
-                    shouldNotGroupWhenFull: false
-                },
-                plugins: [
-                    Alignment,
-                    Autoformat,
-                    AutoImage,
-                    Autosave,
-                    BlockQuote,
-                    Bold,
-                    CKBox,
-                    CKBoxImageEdit,
-                    CloudServices,
-                    Essentials,
-                    FontBackgroundColor,
-                    FontColor,
-                    FontFamily,
-                    FontSize,
-                    FullPage,
-                    GeneralHtmlSupport,
-                    Heading,
-                    Highlight,
-                    HorizontalLine,
-                    HtmlComment,
-                    HtmlEmbed,
-                    ImageBlock,
-                    ImageCaption,
-                    ImageInline,
-                    ImageInsert,
-                    ImageInsertViaUrl,
-                    ImageResize,
-                    ImageStyle,
-                    ImageTextAlternative,
-                    ImageToolbar,
-                    ImageUpload,
-                    Indent,
-                    IndentBlock,
-                    Italic,
-                    Link,
-                    LinkImage,
-                    List,
-                    ListProperties,
-                    MediaEmbed,
-                    Paragraph,
-                    PasteFromOffice,
-                    PictureEditing,
-                    RemoveFormat,
-                    ShowBlocks,
-                    SourceEditing,
-                    Style,
-                    Table,
-                    TableCaption,
-                    TableCellProperties,
-                    TableColumnResize,
-                    TableProperties,
-                    TableToolbar,
-                    TextTransformation,
-                    Title,
-                    TodoList,
-                    Underline
+            toolbar: {
+                items: [
+                    'sourceEditing',
+                    'showBlocks',
+                    '|',
+                    'heading',
+                    'style',
+                    'fontSize',
+                    'fontFamily',
+                    'fontColor',
+                    'fontBackgroundColor',
+                    'bold',
+                    'italic',
+                    'underline',
+                    'link',
+                    'insertImage',
+                    'insertTable',
+                    'highlight',
+                    'blockQuote',
+                    'alignment',
+                    'bulletedList',
+                    'numberedList',
+                    'todoList',
+                    'outdent',
+                    'indent'
                 ],
-                cloudServices: {
-                    tokenUrl: CLOUD_SERVICES_TOKEN_URL
-                },
-                fontFamily: {
-                    supportAllValues: true
-                },
-                fontSize: {
-                    options: [10, 12, 14, 'default', 18, 20, 22],
-                    supportAllValues: true
-                },
-                heading: {
-                    options: [
-                        {
-                            model: 'paragraph' as 'paragraph',
-                            title: 'Paragraph',
-                            class: 'ck-heading_paragraph'
-                        },
-                        {
-                            model: 'heading1' as 'heading1',
-                            view: 'h1',
-                            title: 'Heading 1',
-                            class: 'ck-heading_heading1'
-                        },
-                        {
-                            model: 'heading2' as 'heading2',
-                            view: 'h2',
-                            title: 'Heading 2',
-                            class: 'ck-heading_heading2'
-                        },
-                        {
-                            model: 'heading3' as 'heading3',
-                            view: 'h3',
-                            title: 'Heading 3',
-                            class: 'ck-heading_heading3'
-                        },
-                        {
-                            model: 'heading4' as 'heading4',
-                            view: 'h4',
-                            title: 'Heading 4',
-                            class: 'ck-heading_heading4'
-                        },
-                        {
-                            model: 'heading5' as 'heading5',
-                            view: 'h5',
-                            title: 'Heading 5',
-                            class: 'ck-heading_heading5'
-                        },
-                        {
-                            model: 'heading6' as 'heading6',
-                            view: 'h6',
-                            title: 'Heading 6',
-                            class: 'ck-heading_heading6'
-                        }
-                    ]
-                },
-                htmlSupport: {
-                    allow: [
-                        {
-                            name: /^.*$/,
-                            styles: true,
-                            attributes: true,
-                            classes: [true]
-                        }
-                    ]
-                },
-                image: {
-                    toolbar: [
-                        'toggleImageCaption',
-                        'imageTextAlternative',
-                        'imageStyle:inline',
-                        'imageStyle:wrapText',
-                        'imageStyle:breakText',
-                        'resizeImage',
-                        'ckboxImageEdit'
-                    ]
-                },
-                initialData: content,
-                licenseKey: LICENSE_KEY,
-                link: {
-                    addTargetToExternalLinks: true,
-                    defaultProtocol: 'https://',
-                    decorators: {
-                        toggleDownloadable: {
-                            mode: 'manual',
-                            label: 'Downloadable',
-                            attributes: {
-                                download: 'file'
-                            }
-                        }
+                shouldNotGroupWhenFull: false
+            },
+            plugins: [
+                Alignment,
+                Autoformat,
+                AutoImage,
+                Autosave,
+                BlockQuote,
+                Bold,
+                CKBox,
+                CKBoxImageEdit,
+                CloudServices,
+                Essentials,
+                FontBackgroundColor,
+                FontColor,
+                FontFamily,
+                FontSize,
+                FullPage,
+                GeneralHtmlSupport,
+                Heading,
+                Highlight,
+                HorizontalLine,
+                HtmlComment,
+                HtmlEmbed,
+                ImageBlock,
+                ImageCaption,
+                ImageInline,
+                ImageInsert,
+                ImageInsertViaUrl,
+                ImageResize,
+                ImageStyle,
+                ImageTextAlternative,
+                ImageToolbar,
+                ImageUpload,
+                Indent,
+                IndentBlock,
+                Italic,
+                Link,
+                LinkImage,
+                List,
+                ListProperties,
+                MediaEmbed,
+                Paragraph,
+                PasteFromOffice,
+                PictureEditing,
+                RemoveFormat,
+                ShowBlocks,
+                SourceEditing,
+                Style,
+                Table,
+                TableCaption,
+                TableCellProperties,
+                TableColumnResize,
+                TableProperties,
+                TableToolbar,
+                TextTransformation,
+                Title,
+                TodoList,
+                Underline
+            ],
+            cloudServices: {
+                tokenUrl: CLOUD_SERVICES_TOKEN_URL
+            },
+            fontFamily: {
+                supportAllValues: true
+            },
+            fontSize: {
+                options: [10, 12, 14, 'default', 18, 20, 22],
+                supportAllValues: true
+            },
+            heading: {
+                options: [
+                    {
+                        model: 'paragraph' as 'paragraph',
+                        title: 'Paragraph',
+                        class: 'ck-heading_paragraph'
+                    },
+                    {
+                        model: 'heading1' as 'heading1',
+                        view: 'h1',
+                        title: 'Heading 1',
+                        class: 'ck-heading_heading1'
+                    },
+                    {
+                        model: 'heading2' as 'heading2',
+                        view: 'h2',
+                        title: 'Heading 2',
+                        class: 'ck-heading_heading2'
+                    },
+                    {
+                        model: 'heading3' as 'heading3',
+                        view: 'h3',
+                        title: 'Heading 3',
+                        class: 'ck-heading_heading3'
+                    },
+                    {
+                        model: 'heading4' as 'heading4',
+                        view: 'h4',
+                        title: 'Heading 4',
+                        class: 'ck-heading_heading4'
+                    },
+                    {
+                        model: 'heading5' as 'heading5',
+                        view: 'h5',
+                        title: 'Heading 5',
+                        class: 'ck-heading_heading5'
+                    },
+                    {
+                        model: 'heading6' as 'heading6',
+                        view: 'h6',
+                        title: 'Heading 6',
+                        class: 'ck-heading_heading6'
                     }
-                },
-                list: {
-                    properties: {
+                ]
+            },
+            htmlSupport: {
+                allow: [
+                    {
+                        name: /^.*$/,
                         styles: true,
-                        startIndex: true,
-                        reversed: true
+                        attributes: true,
+                        classes: [true]
                     }
-                },
-                menuBar: {
-                    isVisible: true
-                },
-                placeholder: 'Type or paste your content here!',
-                style: {
-                    definitions: [
-                        {
-                            name: 'Article category',
-                            element: 'h3',
-                            classes: ['category']
-                        },
-                        {
-                            name: 'Title',
-                            element: 'h2',
-                            classes: ['document-title']
-                        },
-                        {
-                            name: 'Subtitle',
-                            element: 'h3',
-                            classes: ['document-subtitle']
-                        },
-                        {
-                            name: 'Info box',
-                            element: 'p',
-                            classes: ['info-box']
-                        },
-                        {
-                            name: 'Side quote',
-                            element: 'blockquote',
-                            classes: ['side-quote']
-                        },
-                        {
-                            name: 'Marker',
-                            element: 'span',
-                            classes: ['marker']
-                        },
-                        {
-                            name: 'Spoiler',
-                            element: 'span',
-                            classes: ['spoiler']
-                        },
-                        {
-                            name: 'Code (dark)',
-                            element: 'pre',
-                            classes: ['fancy-code', 'fancy-code-dark']
-                        },
-                        {
-                            name: 'Code (bright)',
-                            element: 'pre',
-                            classes: ['fancy-code', 'fancy-code-bright']
+                ]
+            },
+            image: {
+                toolbar: [
+                    'toggleImageCaption',
+                    'imageTextAlternative',
+                    'imageStyle:inline',
+                    'imageStyle:wrapText',
+                    'imageStyle:breakText',
+                    'resizeImage',
+                    'ckboxImageEdit'
+                ]
+            },
+            initialData: content,
+            licenseKey: LICENSE_KEY,
+            link: {
+                addTargetToExternalLinks: true,
+                defaultProtocol: 'https://',
+                decorators: {
+                    toggleDownloadable: {
+                        mode: 'manual',
+                        label: 'Downloadable',
+                        attributes: {
+                            download: 'file'
                         }
-                    ]
-                },
-                table: {
-                    contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+                    }
                 }
+            },
+            list: {
+                properties: {
+                    styles: true,
+                    startIndex: true,
+                    reversed: true
+                }
+            },
+            menuBar: {
+                isVisible: true
+            },
+            placeholder: 'Type or paste your content here!',
+            style: {
+                definitions: [
+                    {
+                        name: 'Article category',
+                        element: 'h3',
+                        classes: ['category']
+                    },
+                    {
+                        name: 'Title',
+                        element: 'h2',
+                        classes: ['document-title']
+                    },
+                    {
+                        name: 'Subtitle',
+                        element: 'h3',
+                        classes: ['document-subtitle']
+                    },
+                    {
+                        name: 'Info box',
+                        element: 'p',
+                        classes: ['info-box']
+                    },
+                    {
+                        name: 'Side quote',
+                        element: 'blockquote',
+                        classes: ['side-quote']
+                    },
+                    {
+                        name: 'Marker',
+                        element: 'span',
+                        classes: ['marker']
+                    },
+                    {
+                        name: 'Spoiler',
+                        element: 'span',
+                        classes: ['spoiler']
+                    },
+                    {
+                        name: 'Code (dark)',
+                        element: 'pre',
+                        classes: ['fancy-code', 'fancy-code-dark']
+                    },
+                    {
+                        name: 'Code (bright)',
+                        element: 'pre',
+                        classes: ['fancy-code', 'fancy-code-bright']
+                    }
+                ]
+            },
+            table: {
+                contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
             }
         };
     }, [isLayoutReady]);
@@ -337,7 +335,17 @@ const CKEditorComponent: React.FC<CKEditorComponentProps> = ({ content, setConte
     return (
         <div className="editor-container editor-container_classic-editor editor-container_include-style" ref={editorContainerRef}>
             <div className="editor-container__editor">
-                <div ref={editorRef}>{editorConfig && <CKEditor editor={ClassicEditor} config={editorConfig} data={content} onChange={(event, editor) => setContent(editor.getData())} />}</div>
+                <div ref={editorRef}>
+                    {editorConfig && (
+                        // @ts-ignore
+                        <CKEditor
+                            editor={ClassicEditor}
+                            config={editorConfig}
+                            data={content}
+                            onChange={(event, editor) => setContent(editor.getData())}
+                        />
+                    )}
+                </div>
             </div>
         </div>
     );
