01686a9fee9faa758751ec245bd54e6f1ed842b1
adamn1225 <anoah1225@gmail.com>
2025-01-09 17:06:47 -0500

    auth method revised

--- pages/nts/verify-otp/index.tsx
+++
@@ -1,7 +0,0 @@
-import NtsVerifyOtp from "../../components/NtsVerifyOtp";
-
-const NtsVerifyOtpPage = () => {
-    return <NtsVerifyOtp />;
-}
-
-export default NtsVerifyOtpPage;
---
+++ pages/signup/index.tsx
@@ -0,0 +1,6 @@
+import SignUpPage from '@/pages/components/SignUpPage';
+
+export default function UserSignUp() {
+    return <SignUpPage />
+
+}
