4c8644768ee2d7be0a8f29a999d16ae891daed05
adamn1225 <anoah1225@gmail.com>
2025-01-09 16:57:25 -0500

    auth method revised

--- pages/nts/onboard/index.tsx
+++ pages/nts/onboard/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import NtsSignUp from '../components/NtsSignUp';
+// import React from 'react';
+// import NtsSignUp from '../components/NtsSignUp';
 
-export default function NtsSignUpPage() {
-    return <NtsSignUp />
-}
+// export default function NtsSignUpPage() {
+//     return <NtsSignUp />
+// }
