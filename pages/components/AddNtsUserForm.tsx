490f122860a73d7c86c5690d18ef348437f461a8
adamn1225 <anoah1225@gmail.com>
2025-01-10 10:40:43 -0500

    magic

--- pages/components/AddNtsUserForm.tsx
+++ pages/components/AddNtsUserForm.tsx
@@ -103,7 +103,7 @@ const AddNtsUserForm: React.FC<AddNtsUserFormProps> = ({ isOpen, onClose, onSucc
             const { error: signUpError } = await supabase.auth.signInWithOtp({
                 email: newNtsUser.email,
                 options: {
-                    emailRedirectTo: 'https://shipper-connect.com/nts/nts-set-password',
+                    emailRedirectTo: 'https://www.shipper-connect.com/nts/nts-set-password',
                 },
             });
             
