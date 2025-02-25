8f95ad112ebbd1b2a49f5c2ef1dd60731656856e
adamn1225 <anoah1225@gmail.com>
2025-02-25 12:12:13 -0500

    nanoid

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,20 @@ const nextConfig = {
   reactStrictMode: false,
   productionBrowserSourceMaps: true,
   images: {
-    domains: ['cbarvnrqvxroetrcuikv.supabase.co', 'www.gravatar.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cbarvnrqvxroetrcuikv.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.gravatar.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
