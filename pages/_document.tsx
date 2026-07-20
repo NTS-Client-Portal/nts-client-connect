391fc3a24a39a9a790f6b7f02c78456ee58f87b1
adamn1225 <anoah1225@gmail.com>
2026-07-20 16:18:35 -0400

    context mixup fix;

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/*
+          Favicon setup. Modern browsers pick up the SVG (crisp at any
+          size); the .ico is a fallback for older browsers that ignore
+          type="image/svg+xml".
+        */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="alternate icon" href="/favicon.ico" sizes="any" />
+        <meta name="theme-color" content="#1d4ed8" />
+      </Head>
       <body>
         <Main />
         <NextScript />
